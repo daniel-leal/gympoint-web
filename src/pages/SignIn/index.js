fd816ff250104b0b3124a2687c799696afa1c7e2
Daniel Santos Borges Leal <dleal@banpara.com>
2020-01-27 13:37:05 -0300

    Sign Styles

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Form } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import Label from '~/components/Label';
+import Input from '~/components/Input';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <Container>
+      <img src={logo} alt="GoBarber" />
+
+      <Form>
+        <Label>SEU E-MAIL</Label>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <Label>SUA SENHA</Label>
+        <Input name="password" type="password" placeholder="Sua Senha" />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </Container>
+  );
 }
