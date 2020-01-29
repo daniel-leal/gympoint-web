c7bf3bedf8867c82f1c1af1445a9d251472c803b
daniel-leal <danielleal94@gmail.com>
2020-01-29 17:26:44 -0300

    CRUD Planos

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,9 @@ import Route from './Route';
 import SignIn from '~/pages/SignIn';
 
 import Dashboard from '~/pages/Dashboard';
+
 import PlanList from '~/pages/Plan/List';
+import PlanForm from '~/pages/Plan/Form';
 
 export default function Routes() {
   return (
@@ -13,7 +15,10 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
+
       <Route path="/planos" component={PlanList} isPrivate exact />
+      <Route path="/planos/new" component={PlanForm} isPrivate />
+      <Route path="/planos/:id/edit" component={PlanForm} isPrivate />
     </Switch>
   );
 }
